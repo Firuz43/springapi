a17d63d974509535d6fd6afd128c287b6897755a
Firuz43 <firuz1199@mail.ru>
2023-12-06 23:11:05 +0400

    fetchingapi

--- src/main/java/com/example/springapi/demo/firuz/controller/StudentController.java
+++ src/main/java/com/example/springapi/demo/firuz/controller/StudentController.java
@@ -3,9 +3,12 @@ package com.example.springapi.demo.firuz.controller;
 import com.example.springapi.demo.firuz.entity.Student;
 import com.example.springapi.demo.firuz.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class StudentController {
@@ -18,5 +21,8 @@ public class StudentController {
         return studentService.saveStudent(student);
     }
 
-
+    @GetMapping("/students")
+    public List<Student> fetchStudents() {
+        return studentService.fetchStudents();
+    }
 }
--- src/main/java/com/example/springapi/demo/firuz/service/StudentService.java
+++ src/main/java/com/example/springapi/demo/firuz/service/StudentService.java
@@ -2,6 +2,10 @@ package com.example.springapi.demo.firuz.service;
 
 import com.example.springapi.demo.firuz.entity.Student;
 
+import java.util.List;
+
 public interface StudentService {
     public Student saveStudent(Student student);
+
+    public List<Student> fetchStudents();
 }
